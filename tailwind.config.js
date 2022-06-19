6ea909bf840ca916cad13da695615ab9925613d3
Jose Villanueva <lv428694@gmail.com>
2022-06-19 17:06:08 -0500

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     extend: {
       backgroundImage: theme => ({
         'wave-pattern': 'url(/assets/img/wave-white.png)',
-        'wave-dots': 'url(/assets/img/pattern-white-dots.png)'
+        'wave-dots': 'url(/assets/img/pattern-white-dots.png)',
+        'pattern-characters-red': "url('/assets/img/pattern-characters-red.png')",
+        'character-mario': "url('/assets/img/character-l.png')",
+        'wave-pink': "url('/assets/img/wave-pink.png')",
+        'yellow-dots': "url('/assets/img/pattern-yellow-dots.png')"
       }),
       screens: {
         'xsm': '320px' 
